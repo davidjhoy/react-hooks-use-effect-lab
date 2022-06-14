18be7da369e9323dc44b526036e7c58c5e04580d
david <davidjhoyjr@gmail.com>
2022-06-14 11:40:39 -0400

    commit tree 3af94be80ec528451f4e58ec2536d2e1bab42321

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+// import { useEffect } from "react/cjs/react.production.min";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
-
+  
+  useEffect(
+    // side effect function
+    () => {
+      setTimeout(()=>{
+        let time = timeRemaining
+        if(time === 0){
+          setTimeRemaining(10);
+          onAnswered(false);
+        }else{
+        setTimeRemaining(time - 1);
+        }
+        
+      }, 1000);
+      return () => clearTimeout()
+    },[timeRemaining]
+  );
+  
+  
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
